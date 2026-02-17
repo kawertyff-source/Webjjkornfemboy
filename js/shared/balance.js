75dea48eaeab0f1172f59987b06c89470d8787bf
kawertyff-source <kawertyff@gmail.com>
2026-02-17 20:59:24 +0700

    Create balance.js

---
+++ js/shared/balance.js
@@ -0,0 +1,16 @@
+export const BALANCE = {
+  light:{
+    startup:5,
+    active:3,
+    recovery:10,
+    damage:5,
+    stamina:10
+  },
+  heavy:{
+    startup:12,
+    active:5,
+    recovery:20,
+    damage:12,
+    stamina:25
+  }
+};
